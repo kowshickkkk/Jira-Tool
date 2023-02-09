f531f5a672ad23304930763660e27b4a81446245
kowshickkkk <kowshickkowshick64@gmail.com>
2023-02-09 10:13:01 +0530

    I have added my first code

---
+++ src/project/pojo.java
@@ -0,0 +1,53 @@
+package project;
+
+public class pojo{
+	int Id;
+	String Assignee_name;
+	String Reporter;
+	String Heading;
+	String Status;
+	public pojo(int id, String assignee_name, String reporter, String heading, String status) {
+		super();
+		Id = id;
+		Assignee_name = assignee_name;
+		Reporter = reporter;
+		Heading = heading;
+		Status = status;
+	}
+	
+	public pojo() {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getAssignee_name() {
+		return Assignee_name;
+	}
+	public void setAssignee_name(String assignee_name) {
+		Assignee_name = assignee_name;
+	}
+	public String getReporter() {
+		return Reporter;
+	}
+	public void setReporter(String reporter) {
+		Reporter = reporter;
+	}
+	public String getHeading() {
+		return Heading;
+	}
+	public void setHeading(String heading) {
+		Heading = heading;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}	
+}
